7649e5380fefb4480ac0f8fa3f7dd8eac8c62421
Travis Raghubans <traghubans@traviss-mbp.home>
2020-05-29 16:45:28 -0400

    added php script, moved carosel from four to three, and fixed navbar to make it responsive

---
+++ js/contactForm.js
@@ -0,0 +1,35 @@
+$(document).ready(function() 
+{
+  $("#submit").click(function() 
+  {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    var subject = $("#subject").val();
+    $("#returnmessage").empty(); // To empty previous error/success message.
+  
+    // Checking for blank fields.
+    if (name == '' || email == '' || contact == '') {
+    alert("Please Fill Required Fields");
+  } 
+  else 
+  {
+    // Returns successful data submission message when the entered information is stored in database.
+    $.post("contact_form.php", 
+    {
+      name1: name,
+      email1: email,
+      subject1: subject,
+      message1: message,     
+    }, 
+    function(data) 
+    {
+      $("#returnmessage").append(data); // Append returned message to message paragraph.
+      if (data == "Your Query has been received, We will contact you soon.") 
+      {
+        $("#form")[0].reset(); // To reset form fields on success.
+      }
+    });
+  }
+  });
+  });
